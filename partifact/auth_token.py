6a29ad048abe13f8a3021e4f5917141d8f041ad2
David Steiner <david_j_steiner@yahoo.co.nz>
2021-09-02 13:11:49 +0100

    Use region from configuration when creating boto3 sessions

--- partifact/auth_token.py
+++ partifact/auth_token.py
@@ -14,14 +14,16 @@ def get_token(configuration: Configuration) -> str:
     Returns:
         A valid CodeArtifact token.
     """
-    session = boto3.Session(profile_name=configuration.aws_profile)
+    session = boto3.Session(
+        profile_name=configuration.aws_profile, region_name=configuration.aws_region
+    )
 
     if configuration.aws_role_name:
         role_arn = AWS_ROLE_TEMPLATE.format(
             account=configuration.aws_account,
             role_name=configuration.aws_role_name,
         )
-        session = _assume_role(session, role_arn)
+        session = _assume_role(session, role_arn, configuration.aws_region)
 
     client = session.client("codeartifact")
     response = client.get_authorization_token(
@@ -31,7 +33,7 @@ def get_token(configuration: Configuration) -> str:
     return response["authorizationToken"]
 
 
-def _assume_role(session: boto3.Session, role_arn: str) -> boto3.Session:
+def _assume_role(session: boto3.Session, role_arn: str, region: str) -> boto3.Session:
     client = session.client("sts")
     response = client.assume_role(RoleArn=role_arn, RoleSessionName="partifact-session")
 
@@ -39,4 +41,5 @@ def _assume_role(session: boto3.Session, role_arn: str) -> boto3.Session:
         aws_access_key_id=response["Credentials"]["AccessKeyId"],
         aws_secret_access_key=response["Credentials"]["SecretAccessKey"],
         aws_session_token=response["Credentials"]["SessionToken"],
+        region_name=region,
     )
